41df57367f393fc675728cc109ef5b5ad8f1ebee
Felix Edström <felix.edstrom1@gmail.com>
2025-02-17 19:24:48 +0100

    Fix: Added start command

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/react-weather-app/",
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: process.env.PORT || 4173, // Uses Railway's PORT
+    strictPort: true,
+    host: "0.0.0.0", // Ensures the server is accessible
+  },
 })
